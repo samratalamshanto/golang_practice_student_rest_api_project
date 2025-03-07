fe4284525cea7759caf0bd0754ce4ac504d0efe9
Samrat Alam <samratalamshanto710@gmail.com>
2025-03-07 21:50:53 +0600

    common response added in utility

---
+++ cmd/student-rest-api/utility/common_response.go
@@ -0,0 +1,35 @@
+package utility
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type CommonResponse struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	MSG     string      `json:"msg"`
+	Data    interface{} `json:"data"`
+	Error   interface{} `json:"error"`
+	Err     interface{} `json:"err"`
+}
+
+func SuccessResponse(c *gin.Context, msg string, data interface{}) {
+	c.JSON(http.StatusOK, CommonResponse{
+		Success: true,
+		Message: msg,
+		MSG:     msg,
+		Data:    data,
+		Error:   nil,
+	})
+}
+
+func ErrorResponse(c *gin.Context, msg string, err interface{}) {
+	c.JSON(http.StatusInternalServerError, CommonResponse{
+		Success: false,
+		Message: msg,
+		MSG:     msg,
+		Error:   err,
+		Data:    nil,
+	})
+}
