c583ad2d63fe971aff1a765c57d69e982f978fee
Samrat Alam <samratalamshanto710@gmail.com>
2025-03-08 21:32:00 +0600

    student service transactional practice on going

--- cmd/student-rest-api/service/student_service.go
+++ cmd/student-rest-api/service/student_service.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/samratalamshanto/student_rest_api_project/cmd/student-rest-api/database"
 	"github.com/samratalamshanto/student_rest_api_project/cmd/student-rest-api/models"
+	"gorm.io/gorm"
+	"log"
 )
 
 func GetStudents() (*[]models.Student, error) {
@@ -49,5 +51,51 @@ func GetAllStudentsBySectionAndClass(classId int, section string) (*[]models.Stu
 func DeleteAllStudentsBySectionAndClass(classId int, section string) error {
 	//for update,insert,delete--> no return result
 	res := database.DB.Exec("delete from pp_students where class=? and section=?", classId, section)
+	log.Println("Rows affected:", res.RowsAffected) //Print Rows Affected
 	return res.Error
+}
+
+func TestTransactional() error {
+	tx := database.DB.Begin() //begin transaction
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	var students []models.Student
+	resStd := tx.Where("class=?", 1).Find(&students)
+	if resStd.Error != nil {
+		tx.Rollback()
+		return resStd.Error
+	}
+
+	var teacher models.Teacher
+	resTeacher := tx.Where("name=?", "Sam").Find(&teacher) // For transactional, should use same tx, not database.DB (create new tx)
+	if resTeacher.Error != nil {
+		tx.Rollback()
+		return resTeacher.Error
+	}
+
+	return tx.Commit().Error
+}
+
+func TestTransactionalByGorm() error {
+	return database.DB.Transaction(func(tx *gorm.DB) error {
+
+		var students []models.Student
+		resStd := tx.Where("class=?", 1).Find(&students)
+		if resStd.Error != nil {
+			return resStd.Error
+		}
+
+		var teacher models.Teacher
+		resTeacher := tx.Where("name=?", "Sam").Find(&teacher) // For transactional, should use same tx, not database.DB (create new tx)
+		if resTeacher.Error != nil {
+			return resTeacher.Error
+		}
+
+		return nil
+	})
 }
